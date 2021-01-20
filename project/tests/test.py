bf5e9490372b2489b4a190d41d7c845a780374f9
kei_sakuma <kei39maroond@yahoo.co.jp>
2021-01-20 18:11:01 +0900

    Add initial unit test file

---
+++ project/tests/test.py
@@ -0,0 +1,43 @@
+import unittest
+ 
+from project import app
+ 
+ 
+class ProjectTests(unittest.TestCase):
+ 
+    ############################
+    #### setup and teardown ####
+    ############################
+ 
+    # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+ 
+        self.assertEquals(app.debug, False)
+ 
+    # executed after each test
+    def tearDown(self):
+        pass
+ 
+ 
+    ########################
+    #### helper methods ####
+    ########################
+ 
+ 
+ 
+    ###############
+    #### tests ####
+    ###############
+ 
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertIn(b'Welcome to the Sneaker App!', response.data)
+        self.assertIn(b'This site is sneaker shelf!', response.data)
+        self.assertIn(b'Adidas', response.data)
+        self.assertIn(b'Nike', response.data)
+ 
+if __name__ == "__main__":
+    unittest.main()
