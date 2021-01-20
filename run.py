2f89174d31b8e76bd851a24471928c96d8f16118
kei_sakuma <kei39maroond@yahoo.co.jp>
2021-01-20 18:16:03 +0900

    Added use of instance folder

--- run.py
+++ run.py
@@ -1,7 +1,4 @@
 
-# Add your settings here... this is a temporary location, as the settings for a Flask app
-# should be stored separate from your main program.
-DEBUG = True
  
 from project import app
  
