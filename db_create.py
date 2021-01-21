cce6221994eb162f8947a27aa3c5f1171d49a600
kei_sakuma <kei39maroond@yahoo.co.jp>
2021-01-21 16:53:56 +0900

    Added password hashing

--- db_create.py
+++ db_create.py
@@ -1,5 +1,5 @@
 from project import db
-from project.models import Sneaker
+from project.models import Sneaker,User
 
 
 # drop all of the existing database tables
@@ -11,9 +11,16 @@ db.create_all()
 sneaker1 = Sneaker('adidas don issue 2 white black gold', 10000)
 sneaker2 = Sneaker('adidas nmd r1 star wars the mandalorian', 14000)
 sneaker3 = Sneaker('adidas top ten hi star wars the mandalorian the child', 9000)
-
 db.session.add(sneaker1)
 db.session.add(sneaker2)
 db.session.add(sneaker3)
+
+# insert user data
+user1 = User('patkennedy79@gmail.com', 'password1234')
+user2 = User('kennedyfamilyrecipes@gmail.com', 'PaSsWoRd')
+user3 = User('blaa@blaa.com', 'MyFavPassword')
+db.session.add(user1)
+db.session.add(user2)
+db.session.add(user3)
 
 db.session.commit()
